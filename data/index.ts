fc9f279f9336c7b6b07340acdc89112c61ae9131
mister-stan <lucianstan87@yahoo.com>
2024-06-03 03:57:08 +0100

    adds the app's scaffolding code

---
+++ data/index.ts
@@ -0,0 +1,227 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building an E-commerce website.",
+      description: "E-commerce website using Next JS, Tailwind CSS, and Stripe for payments.",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "3D Solar System Planets to Explore",
+      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+      img: "/p1.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://github.com/Mister-Stan/3d-solar-system-planets",
+    },
+    {
+      id: 2,
+      title: "Yoom - Video Conferencing App",
+      des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+      img: "/p2.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://github.com/Mister-Stan/zoom-clone",
+    },
+    {
+      id: 3,
+      title: "AI Image SaaS - Canva Application",
+      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+      img: "/p3.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://github.com/Mister-Stan/ai-canva-saas",
+    },
+    {
+      id: 4,
+      title: "Animated Apple Iphone 3D Website",
+      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/Mister-Stan/3d-apple-iphone",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote: "The website Eugeniu crafted for our company blended modern design with a great user experience perfectly. Highly recommended for innovative web solutions!",
+      name: "Samantha Lewis",
+      title: "Marketing Director"
+    },
+    {
+      quote: "Eugeniu's talents are remarkable and he delivered a compelling online presence that combined his coding expertise with artistic flair for our brand.",
+      name: "Michael Woodson",
+      title: "Technology Advisor"
+    },
+    {
+      quote: "Eugeniu effortlessly blended aesthetics and functionality, creating an engaging online experience that captivated our audience. A true master of optimization and user experience.",
+      name: "Christopher Blake",
+      title: "Senior Consulting Manager"
+    },
+    {
+      quote: "Eugeniu's proficiency in optimizing online engagements is astounding. His digital artistry and seamless user experiences are truly impressive.",
+      name: "Ashley Collins",
+      title: "Communications Director"
+    },
+    {
+      quote: "Eugeniu's coding prowess converges seamlessly with his creative flair. His ability to craft visually stunning and highly optimized online presences is impressive.",
+      name: "Michael Vance",
+      title: "Senior Marketing Consultant"
+    },
+    {
+      quote: "With his innovation, foresight, and deep grasp of modern web technologies, Eugeniu is invaluable for any digital initiative or creative vision.",
+      name: "Christopher Jones",
+      title: "Chief Technology Consultant"
+    },
+    {
+      quote: "Eugeniu's web solutions consistently deliver engaging and visually striking online experiences that capture the essence of a brand.",
+      name: "Samantha Williams",
+      title: "Marketing Strategist"
+    },
+    {
+      quote: "Eugeniu's technical expertise and keen eye for aesthetic detail are remarkable. The online experiences he crafts captivate audiences.",
+      name: "Michael Thompson",
+      title: "Digital Marketing Specialist"
+    }
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Web Developer",
+      desc: "Build and maintain a popular tattoo shop website focusing on user experience and performance.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Mobile App Developer",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance Web Developer",
+      desc: "Build and maintain a website application for an animal shelter in Romania that collaborates with a British animal association to facilitate adoptions from the Romanian shelter to the United Kingdom.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
